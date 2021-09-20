67565d1c98b26ad943075cbd0eba59f29bcc45ac
Gerrizone <91075176+Gerrizone@users.noreply.github.com>
2021-09-20 17:54:56 +0200

    pxt.json, main.blocks, main.ts, main.py frissítése

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-let szam = 0
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
+szam = 0
+
+def on_gesture_shake():
+    global szam
     szam = randint(0, 2)
-    if (szam == 0) {
-        basic.showLeds(`
+    if szam == 0:
+        basic.show_leds("""
             . . . . .
                         . . . . .
                         . . # . .
                         . . . . .
                         . . . . .
-        `)
-    } else if (szam == 1) {
-        basic.showLeds(`
+        """)
+    elif szam == 1:
+        basic.show_leds("""
             # # # # #
                         # . . . #
                         # . . . #
                         # . . . #
                         # # # # #
-        `)
-    } else {
-        basic.showLeds(`
+        """)
+    else:
+        basic.show_leds("""
             # . . # .
                         # . # . .
                         # # . . .
                         # . # . .
                         # . . # .
-        `)
-    }
-    
-})
+        """)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
